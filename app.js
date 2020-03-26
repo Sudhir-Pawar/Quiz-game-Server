47ca34da6f7969dac18cf3701a907176ba47e0ac
sudhir <pawarsudhir84@gmail.com>
2020-03-26 18:19:38 +0530

    resolve errors and updated post

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port=9000;
 const app=express();
 app.use(bodyparser.urlencoded({extended : true}));
 
-mongoose.connect("mongodb://localhost:27017/QuizDB",{ useNewUrlParser: true } ,function(err){
+mongoose.connect("mongodb://localhost:27017/QuizDB",{  useUnifiedTopology: true ,useNewUrlParser: true } ,function(err){
     if(!err)
         console.log("Connnect to database");
     else
@@ -28,19 +28,24 @@ app.get("/questions",function(req,res){
 });
 
 app.post("/questions",function(req,res){
-    const qTemp=new Question({
-        question : req.body.question,
-        options : req.body.options
-    });
 
-    qTemp.save(function(err){
-        err ? console.log(err) : console.log("Question added to database");
-    })
+    if(req.body.options.length > 4) 
+         res.send("options size limited to 4") 
+    else{
+        const qTemp=new Question({
+            question : req.body.question,
+            options : req.body.options
+        });
+
+        qTemp.save(function(err){
+            err ? console.log(err) : res.send("Question added to database");
+        })
+    }
 });
 
 app.delete("/questions",function(req,res){
     Question.deleteMany(function(err){
-        err ? console.log(err) : console.log("All questions deleted");
+        err ? console.log(err) : res.send("All questions deleted");
     });
 });
 
